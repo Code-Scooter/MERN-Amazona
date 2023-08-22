57ab45899306e3081685f987e2fc02bf435f7f5a
957023621 <957023621@qq.com>
2023-08-22 10:33:08 -0400

    update the function of fetch data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
